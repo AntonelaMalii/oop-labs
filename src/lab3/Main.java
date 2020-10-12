48539d4511f4c20dab2419df22151abf907d18c7
AntonelaMalii <malii.antonela@isa.utm.md>
2020-10-12 23:45:15 +0300

    init commit

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -1,11 +1,24 @@
 package lab3;
 
+
 public class Main {
     public static void main(String[] args) {
 
-        TextManipulator textm= new TextManipulator();
 
-        textm.getNrVowels("Aceasta este o zi frumoasa.Maine va fi una si mai frumoasa.");
+TextManipulator tx= new TextManipulator();
+tx.createText();
+String text=tx.getText();
+        System.out.println("Nr de cuvinte in text:");
+        System.out.println(tx.countWords(text));
+        System.out.println("Nr de propozitii in text:");
+        System.out.println(tx.nrOfSequences(text));
+        System.out.println("Nr de vocale in text:");
+        System.out.println(tx.countVowels(text));
+        System.out.println("Nr de consoane in text:");
+        System.out.println(tx.countConsonants(text));
+        System.out.println("Top 5 cuvinte cele mai frecvente: ");
+        tx.occurenceCount(tx.getText());
+
 
     }
 }
--- src/lab3/TextManipulator.java
+++ src/lab3/TextManipulator.java
@@ -1,24 +1,106 @@
 package lab3;
 
-public class TextManipulator {
-    private String text;
-    private String[] tokens;
-    private String[] sentence;
+import javafx.util.Pair;
+import java.util.*;
+import java.util.stream.Collectors;
 
+
+public class TextManipulator {
+public String str;
+
+Scanner scanner1=new Scanner(System.in);
+
+    public void createText() {
+        System.out.println("Give the text:");
+            this.str =scanner1.nextLine();
+
+    }
 
     public String getText() {
-        return text;
-    }
-    public void setText(){
-        this.text=text;
+        return this.str;
     }
 
-    public void getNrVowels(String text){
-        this.tokens=text.split(" ");
-        this.sentence=text.split(".");
 
-        System.out.println(tokens.length);
-        System.out.println(sentence.length);
 
+public void occurenceCount(String text) {
+    LinkedHashMap<String,Integer> words = new LinkedHashMap<String,Integer>();
+    String str1 [] = text.split("[\\s.,?!:]");
+    for (String s : str1){
+        int count=0;
+        if(words.containsKey(s)){
+            count = words.get(s);
+        }
+        words.put(s,count+1);
     }
+
+    List<Pair<String,Integer>> mostFrequent=
+            words.entrySet()
+                  .stream()
+                  .sorted(Comparator.comparing(e->-e.getValue()))
+                   .map(e->new Pair<>(e.getKey(),e.getValue()))
+                   .collect(Collectors.toList());
+int c=0;
+for(Pair<String,Integer> e : mostFrequent){
+
+    if(c++>=5) break;
+    System.out.println(e.getKey() + " : " + e.getValue());
+
+}
+
+}
+
+
+    public static int countWords(String text){
+        if(text == null || text.isEmpty()){
+            return 0;
+        }else {
+            String[] tokens= text.split("[\\s.,?!:]+");
+            return tokens.length;
+        }
+}
+    public static int nrOfSequences(String text){
+        if(text == null || text.isEmpty()){
+            return 0;
+        }else {
+            String[] tokens= text.split("[!?.:]+");
+            return tokens.length;
+        }
+    }
+    public static int countVowels(String text)
+    {
+        int count = 0;
+        text=text.toLowerCase();
+        for (int i = 0; i < text.length(); i++)
+        {
+            if (text.charAt(i) == 'a' || text.charAt(i) == 'e' || text.charAt(i) == 'i'
+                    || text.charAt(i) == 'o' || text.charAt(i) == 'u')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static int countConsonants(String text)
+    {
+        int countv = 0;
+        int countc=0;
+        text=text.toLowerCase();
+        for (int i = 0; i < text.length(); i++)
+        {
+            if (text.charAt(i) == 'a' || text.charAt(i) == 'e' || text.charAt(i) == 'i'
+                    || text.charAt(i) == 'o' || text.charAt(i) == 'u')
+            {
+                countv++;
+            }else if(text.charAt(i)>='a' && text.charAt(i)<='z'){
+                countc++;
+            }
+        }
+        return countc;
+    }
+
+
+
+
+
+
 }
